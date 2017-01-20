f0d11e4ad9adfaf1d18f382c6b68664027742756
Geoff Hackett <ghackett@gmail.com>
2017-01-19 20:52:39 -0500

    Make the plugin test do something close to what it eventually will

--- src/test/groovy/com/episode6/hackit/gdmc/GdmcPluginTest.groovy
+++ src/test/groovy/com/episode6/hackit/gdmc/GdmcPluginTest.groovy
@@ -1,5 +1,9 @@
 package com.episode6.hackit.gdmc
 
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
 
 /**
@@ -7,11 +11,66 @@ import spock.lang.Specification
  */
 class GdmcPluginTest extends Specification {
 
+  @Rule final TemporaryFolder buildFolder = new TemporaryFolder()
+
   def "placeholder test"() {
+    given:
+    buildFolder.newFile("build.gradle") << """
+plugins {
+  id 'java'
+  id 'com.episode6.hackit.gdmc'
+}
+
+group = 'com.example.testproject'
+version = '0.0.1-SNAPSHOT'
+
+repositories {
+  jcenter()
+}
+
+dependencies {
+  // The goal is to use something like this commented format,
+  // but I don't want the tests to fail yet
+  // compile gdmc('com.episode6.hackit.chop:chop-core')
+  compile 'com.episode6.hackit.chop:chop-core:0.1.7.2'
+}
+"""
+    createNonEmptyJavaFile("com.episode6.testproject")
+
     when:
-    def result = true
+    def result = GradleRunner.create()
+        .withProjectDir(buildFolder.root)
+        .withPluginClasspath()
+        .withArguments("build")
+        .build()
 
     then:
-    result
+    result.task(":build").outcome == TaskOutcome.SUCCESS
+  }
+
+  File createNonEmptyJavaFile(String packageName, String className = "SampleClass", File rootDir = buildFolder.getRoot()) {
+    File dir = rootDir
+    "src.main.java.${packageName}".tokenize(".").each {
+      dir = new File(dir, it)
+    }
+    dir.mkdirs()
+    File nonEmptyJavaFile = new File(dir, "${className}.java")
+    nonEmptyJavaFile << """
+package ${packageName};
+
+import com.episode6.hackit.chop.Chop;
+
+/**
+ * A sample class for testing
+ */
+public class ${className} {
+
+  public void sendMessage() {
+    Chop.i("hello");
+  }
+
+}
+"""
+    return nonEmptyJavaFile
   }
 }
