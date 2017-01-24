abe751555301b8a2ae5f99f4c35a4c93490e1ad6
Geoff Hackett <ghackett@gmail.com>
2017-01-24 17:42:41 -0500

    add a multi-project test. It currently fails because we’re maintaining multiple versions of the dependency map so it gets overwritten

--- src/test/groovy/com/episode6/hackit/gdmc/GdmcResolveTest.groovy
+++ src/test/groovy/com/episode6/hackit/gdmc/GdmcResolveTest.groovy
@@ -1,6 +1,7 @@
 package com.episode6.hackit.gdmc
 
 import com.episode6.hackit.gdmc.testutil.IntegrationTest
+import com.episode6.hackit.gdmc.testutil.IntegrationTest.SubProject
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.Rule
 import spock.lang.Specification
@@ -46,6 +47,76 @@ dependencies {
     then:
     result.task(":gdmcResolve").outcome == TaskOutcome.SUCCESS
     test.gdmcJsonFile.exists()
+    with(test.gdmcJsonFile.asJson()) {
+      with(get("org.mockito:mockito-core")) {
+        groupId == "org.mockito"
+        artifactId == "mockito-core"
+        !version.contains("-SNAPSHOT")
+      }
+      with(get("com.episode6.hackit.chop:chop-core")) {
+        groupId == "com.episode6.hackit.chop"
+        artifactId == "chop-core"
+        !version.contains("-SNAPSHOT")
+      }
+      with(get("org.spockframework:spock-core")) {
+        groupId == "org.spockframework"
+        artifactId == "spock-core"
+        !version.contains("-SNAPSHOT")
+      }
+      verifyJsonSortOrder((Map)delegate)
+    }
+  }
+
+  def "mutli-project test"() {
+    given:
+    test.gdmcJsonFile << "{}"
+    test.gradleBuildFile << """
+allprojects {
+  group = "com.example"
+  version = "0.0.1-SNAPSHOT"
+  
+  repositories {
+    jcenter()
+  }
+}
+"""
+    with(test.subProject("javalib")) {
+      gradleBuildFile << """
+plugins {
+  id 'java'
+  id 'com.episode6.hackit.gdmc'
+}
+
+dependencies {
+   compile gdmc('com.episode6.hackit.chop:chop-core')
+}
+"""
+    }
+    with(test.subProject("groovylib")) {
+      gradleBuildFile << """
+plugins {
+  id 'groovy'
+  id 'com.episode6.hackit.gdmc'
+}
+dependencies {
+   compile gdmc('com.episode6.hackit.chop:chop-core')
+   testCompile(gdmc(group: 'org.spockframework', name: 'spock-core'))  {
+    exclude module: 'groovy-all'
+  }
+}
+"""
+    }
+
+    when:
+    def result = test.runTask("gdmcResolve")
+
+    then:
+    result.task(":javalib:gdmcResolve").outcome == TaskOutcome.SUCCESS
+    result.task(":groovylib:gdmcResolve").outcome == TaskOutcome.SUCCESS
+    test.gdmcJsonFile.exists()
+
+    println "gdmcFile text: ${test.gdmcJsonFile.text}"
+
     with(test.gdmcJsonFile.asJson()) {
       with(get("org.mockito:mockito-core")) {
         groupId == "org.mockito"
