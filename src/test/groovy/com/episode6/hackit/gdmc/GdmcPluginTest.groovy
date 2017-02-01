dd30e1859cf8228fb4153eb60db01d2ff9e4d6df
Geoff Hackett <ghackett@gmail.com>
2017-01-31 20:15:36 -0500

    refactor the main plugin test

--- src/test/groovy/com/episode6/hackit/gdmc/GdmcPluginTest.groovy
+++ src/test/groovy/com/episode6/hackit/gdmc/GdmcPluginTest.groovy
@@ -9,44 +9,36 @@ import spock.lang.Specification
  * Tests {@link GdmcPlugin}
  */
 class GdmcPluginTest extends Specification {
-  private static final CHOP_IMPORT = """
+  private static final String CHOP_IMPORT = """
 import com.episode6.hackit.chop.Chop;
 
+"""
+  private static final String SPOCK_IMPORT = """
+import spock.lang.Specification;
 
 """
+  private static final String MOCKITO_IMPORT = """
+import org.mockito.Mockito;
 
-  @Rule final IntegrationTest test = new IntegrationTest()
-
-  def setup() {
-    test.createJavaFile(packageName: "com.episode6.testproject", imports: CHOP_IMPORT)
-    test.createJavaFile(packageName: "com.episode6.testproject", className: "SampleClassTest", dir: "test")
-  }
-
-  def "test resolve pre-set dependencies"() {
-    given:
-    test.gdmcJsonFile << """
-{
-  "chop": {
-    "alias": [
-      "com.episode6.hackit.chop:chop-core"
-    ]
-  },
-  "grouptest": {
-    "alias": "chop"
-  },
+"""
+  private static final String PRE_SET_DEPENDENCIES = """
   "com.episode6.hackit.chop:chop-core": {
       "groupId": "com.episode6.hackit.chop",
       "artifactId": "chop-core",
       "version": "0.1.7.2"
+   },
+   "org.mockito:mockito-core": {
+     "groupId": "org.mockito",
+     "artifactId": "mockito-core",
+     "version": "2.7.0"
    },
    "org.spockframework:spock-core": {
      "groupId": "org.spockframework",
      "artifactId": "spock-core",
      "version": "1.1-groovy-2.4-rc-2"
    }
-}
 """
-    test.gradleBuildFile << """
+  private static final String BUILD_FILE_PREFIX = """
 plugins {
   id 'groovy'
   id 'com.episode6.hackit.gdmc'
@@ -59,14 +51,32 @@ repositories {
   jcenter()
 }
 
+"""
+
+  @Rule final IntegrationTest test = new IntegrationTest()
+
+  def setup() {
+    test.gradleBuildFile << BUILD_FILE_PREFIX
+    test.createJavaFile(packageName: "com.episode6.testproject", imports: CHOP_IMPORT)
+    test.createJavaFile(packageName: "com.episode6.testproject", className: "SampleClassTest", dir: "test", imports: SPOCK_IMPORT)
+  }
+
+
+  def "test resolve pre-set dependencies simple"() {
+    given:
+    test.gradleBuildFile << """
 dependencies {
-   compile gdmc('grouptest')
+   compile 'com.episode6.hackit.chop:chop-core'
    testCompile(group: 'org.spockframework', name: 'spock-core')  {
     exclude module: 'groovy-all'
   }
 }
 """
-
+    test.gdmcJsonFile << """
+{
+${PRE_SET_DEPENDENCIES}
+}
+"""
     when:
     def result = test.runTask("build")
 
@@ -74,26 +84,51 @@ dependencies {
     result.task(":build").outcome == TaskOutcome.SUCCESS
   }
 
+
+  def "test resolve pre-set dependencies aliases"() {
+    given:
+    test.createJavaFile(packageName: "com.episode6.testproject", imports: MOCKITO_IMPORT, className: "SampleClass2")
+    test.gradleBuildFile << """
+dependencies {
+   compile gdmc('subgroup')
+   testCompile(group: 'org.spockframework', name: 'spock-core')  {
+    exclude module: 'groovy-all'
+  }
+}
+"""
+    test.gdmcJsonFile << """
+{
+  "biggroup": {
+    "alias": [
+      "com.episode6.hackit.chop:chop-core",
+      "mockito"
+    ]
+  },
+  "subgroup": {
+    "alias": "biggroup"
+  },
+  "mockito": {
+    "alias": "org.mockito:mockito-core"
+  },
+${PRE_SET_DEPENDENCIES}
+}
+"""
+    when:
+    def result = test.runTask("build")
+
+    then:
+    result.task(":build").outcome == TaskOutcome.SUCCESS
+  }
+
+
+
   def "test failure on missing dependency"() {
     given:
     test.gradleBuildFile << """
-plugins {
-  id 'groovy'
-  id 'com.episode6.hackit.gdmc'
-}
-
-group = 'com.example.testproject'
-version = '0.0.1-SNAPSHOT'
-
-repositories {
-  jcenter()
-}
-
 dependencies {
    compile 'com.episode6.hackit.chop:chop-core'
 }
 """
-
     when:
     def result = test.runTaskAndFail("build")
 
