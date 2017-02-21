d2a364c866708ce0f882bf11415d7295f9ebea98
Geoff Hackett <ghackett@gmail.com>
2017-02-21 10:19:19 -0500

    clean up how we access overwrite property

--- src/main/groovy/com/episode6/hackit/gdmc/util/ProjectProperties.groovy
+++ src/main/groovy/com/episode6/hackit/gdmc/util/ProjectProperties.groovy
@@ -7,7 +7,9 @@ import org.gradle.api.Project
  */
 class ProjectProperties {
 
+  private static final String OVERWRITE = "overwrite"
+
   static boolean overwrite(Project project) {
-    return project.hasProperty("overwrite") && project.overwrite
+    return project.hasProperty(OVERWRITE) && project.property(OVERWRITE)
   }
 }
