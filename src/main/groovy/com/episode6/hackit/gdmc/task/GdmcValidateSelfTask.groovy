91bc83db0679357e4a74bde3326b4451a7d38e94
Geoff Hackett <ghackett@gmail.com>
2017-02-27 11:17:20 -0500

    make GdmcValidateSelf task implement VerificationTask and implement ignoreFailures

--- src/main/groovy/com/episode6/hackit/gdmc/task/GdmcValidateSelfTask.groovy
+++ src/main/groovy/com/episode6/hackit/gdmc/task/GdmcValidateSelfTask.groovy
@@ -7,20 +7,24 @@ import com.episode6.hackit.gdmc.plugin.GdmcRootPlugin
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.VerificationTask
 
 import static com.episode6.hackit.gdmc.util.GdmcLogger.GChop
 
 /**
  * Task that ensures your project's gdmc file always references it's current version
  */
-class GdmcValidateSelfTask extends DefaultTask {
+class GdmcValidateSelfTask extends DefaultTask implements VerificationTask {
 
   @Input Closure<Boolean> required = {!project.version.contains("SNAPSHOT")}
 
+  @Input boolean ignoreFailures = false
+
   @TaskAction
   def validate() {
+    String taskName = name
     if (!required.call()) {
-      GChop.d("Validation not required, skipping task: ${name}")
+      GChop.d("Validation not required, skipping task: ${taskName}")
       return
     }
 
@@ -31,9 +35,13 @@ class GdmcValidateSelfTask extends DefaultTask {
       if (size() == 1 && get(0) == selfDep) {
         GChop.d("Succesfully validated ${selfDep} in gdmc.")
       } else {
-        throw new GdmcSelfValidationException(selfDep, delegate)
+        GdmcSelfValidationException failure = new GdmcSelfValidationException(selfDep, delegate)
+        if (ignoreFailures) {
+          GChop.e(failure, "Ignoring failure in task %s", taskName)
+        } else {
+          throw failure
+        }
       }
     }
   }
-
 }
